b16ee56f9bb729bf3e2bf36e9acc8ad8cca06e06
kunaltyagi18 <tyagikunal1818@gmail.com>
2025-08-03 19:45:59 +0530

    Update vite.config.js

--- Todo_frontend/vite.config.js
+++ Todo_frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // This should be correct if index.html is in the root
 })
